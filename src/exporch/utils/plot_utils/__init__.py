d1f138b1a1e9a2354a1c8cc281c1812719521e87
enricosimionato <enrico.simionato@mail.polimi.it>
2024-09-16 11:04:53 +0200

    Bug fic

--- src/exporch/utils/plot_utils/__init__.py
+++ src/exporch/utils/plot_utils/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "set_axis_labels"
 ]
 
-from exporch.utils.plot_utils.plot_utils import plot_heatmap, plot_heatmap_with_additional_row_column, set_axis_labels
+from exporch.utils.plot_utils.heatmap import plot_heatmap, plot_heatmap_with_additional_row_column, set_axis_labels
