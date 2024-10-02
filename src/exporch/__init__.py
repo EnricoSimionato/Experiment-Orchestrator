a896490f18bf8b423472636fdde241092c118aca
enricosimionato <enrico.simionato@mail.polimi.it>
2024-10-02 09:43:12 +0200

    Fixing imports

--- src/exporch/__init__.py
+++ src/exporch/__init__.py
@@ -8,7 +8,7 @@ __all__ = [
     "Verbose",
 
     "get_available_device",
-    "check_path_to_storage"
+    "check_path_to_storage",
 ]
 
 from exporch.configuration.config import Config
@@ -18,5 +18,3 @@ from exporch.utils.print_utils.print_utils import Verbose
 
 from exporch.utils.device_utils.device_utils import get_available_device
 from exporch.utils.storage_utils.storage_utils import check_path_to_storage
-
-from exporch.experiment.benchmarking_experiment import evaluate_model_on_benchmark
